47ba0904069232bc3f70a49c233eb4cf053cbd69
Gustavo <gustavobaister@gmail.com>
2026-05-06 21:55:05 -0300

    feat.: criando implements e daoimplements

--- SistemaEscolar/src/main/java/dao/IAlunoDAO.java
+++ SistemaEscolar/src/main/java/dao/IAlunoDAO.java
@@ -1,7 +1,20 @@
 package dao;
 
+import model.Aluno;
+
+import java.util.List;
+
 public interface IAlunoDAO {
     //CRUD
     // create
+    void salvarAluno(Aluno aluno);
 
+    //Read
+    List<Aluno> listarTodosAlunos();
+
+    //Update
+    void atualizarAluno(Aluno aluno);
+
+    //Delete
+    void excluirAluno(Aluno aluno);
 }
