5da5a6eddb718dfb9a8848bf0042f5a9561bfb5e
Gustavo <gustavobaister@gmail.com>
2026-05-07 19:44:38 -0300

    feat: criando listarTodosOsAlunos

--- SistemaEscolar/src/main/java/daoImplements/AlunoDAOImplements.java
+++ SistemaEscolar/src/main/java/daoImplements/AlunoDAOImplements.java
@@ -1,8 +1,14 @@
 package daoImplements;
 
 import dao.IAlunoDAO;
+import database.sqlConn;
 import model.Aluno;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class AlunoDAOImplements implements IAlunoDAO {
@@ -13,7 +19,16 @@ public class AlunoDAOImplements implements IAlunoDAO {
 
     @Override
     public List<Aluno> listarTodosAlunos() {
-        return List.of();
+        String sql = "SELECT * FROM aluno ORDER BY nome ASC";
+        List<Aluno> alunos = new ArrayList<>();
+
+        try (Connection conn = sqlConn.getConnection()){
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            ResultSet rs = stmt.executeQuery();
+        }catch (SQLException e){
+            System.err.print("Erro ao listar os alunos: " + e.getMessage());
+        }
+        return alunos;
     }
 
     @Override
